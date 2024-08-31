ef4d48f42a97aa861e9d3bab7e7d057f1a914cd4
Vladimir Golovko <g-vn90@mail.ru>
2024-08-31 21:04:34 +0300

    modife function is_prime in prime.py

--- games/prime.py
+++ games/prime.py
@@ -5,12 +5,13 @@ from games.greeting import greeting
 def is_prime(number):
     if number <= 1:
         return False
-    if number <= 3:
+    if number == 2:
         return True
-    if number % 2 == 0 or number % 3 == 0:
+    if number % 2 == 0:
         return False
-    for i in range(5, int(number**0.5) + 1, 6):
-        if number % i == 0 or number % (i + 2) == 0:
+    limit = int(number ** 0.5) + 1
+    for i in range(3, limit, 2):
+        if number % i == 0:
             return False
     return True
 
